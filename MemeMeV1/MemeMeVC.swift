a6c601e28d552a3971712f92f87224178a7688e1
Chuck Underwood <chillieguy@live.com>
2018-05-29 06:01:25 -0700

    Implemented basic textfield editing and formatting

--- MemeMeV1/MemeMeVC.swift
+++ MemeMeV1/MemeMeVC.swift
@@ -14,19 +14,39 @@ class MemeMeVC: UIViewController {
     
     
     // Mark - @IBOutlets
-    @IBOutlet weak var topLabel: UILabel!
-    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     
-    // Mark - ViewDidLoad
+    // Mark - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
        
         // Camera enabled if available
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        // Set UITextField delegate
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        // Set UITextField formatting
+        setTextFormatting()
     }
+    
+    // Mark - viewWillAppear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    // Mark - viewWillDisappear
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        unsubscribeToKeyboardNotifications()
+    }
+    
 
     // Mark - @IBActions
     @IBAction func shareAction(_ sender: Any) {
@@ -54,7 +74,51 @@ class MemeMeVC: UIViewController {
     }
     
     // Mark - Helper Functions
+    
+    func setTextFormatting() {
+        let textFieldAttributes: [String: Any] = [
+            NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+            NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+            NSAttributedStringKey.strokeWidth.rawValue: -3,
+            NSAttributedStringKey.font.rawValue: UIFont(name: "Impact", size: 40)!
+            
+        ]
+        
+        topTextField.defaultTextAttributes = textFieldAttributes
+        bottomTextField.defaultTextAttributes = textFieldAttributes
+        
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
 }
+
+// Mark - UIImagePickerControllerDelegate
 
 extension MemeMeVC: UIImagePickerControllerDelegate {
     
@@ -63,13 +127,37 @@ extension MemeMeVC: UIImagePickerControllerDelegate {
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        print(info)
         imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         dismiss(animated: true, completion: nil)
     }
 }
 
+// Mark - UINavigationControllerDelegate
+
 extension MemeMeVC: UINavigationControllerDelegate {
+    
+}
+
+// Mark - UITextFieldDelegate
+
+extension MemeMeVC: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+        
+        textField.layer.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.backgroundColor = nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
 }
 
